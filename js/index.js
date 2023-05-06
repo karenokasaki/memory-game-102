4d76f0b30fa1cdcfdad0b5ec16916f10960b1a95
karen okasaki <oks.karen@gmail.com>
2023-05-06 11:32:55 -0300

    deck

--- js/index.js
+++ js/index.js
@@ -1,8 +1,20 @@
 //capturar os elementos de html
-
 const homeScreen = document.querySelector("#homeScreen");
 const buttonStart = document.querySelector("#buttonStart");
 const gameScrren = document.querySelector("#gameScreen");
+const board = document.querySelector("#board");
+
+// variáveis
+const deck = [
+  "./images/harmonia.svg",
+  "./images/poder.svg",
+  "./images/projetar.svg",
+  "./images/refletir.svg",
+  "./images/harmonia.svg",
+  "./images/poder.svg",
+  "./images/projetar.svg",
+  "./images/refletir.svg",
+];
 
 buttonStart.addEventListener("click", () => {
   // o que é para acontecer quando eu clicar no botão jogar?
@@ -14,4 +26,11 @@ buttonStart.addEventListener("click", () => {
   // mostrar a gameScreen
   // como? -> removendo a classe hide da div
   gameScrren.classList.remove("hide");
+
+
 });
+
+
+function startGame() {
+    
+}
